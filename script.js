6ac4921220dea9e8527bf5f31a50b70263bc1256
Stavros <101610510@student.swin.edu.au>
2021-04-19 15:38:13 +1000

    made a calculator

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function ChangeText() {
-    document.getElementById("demo").innerHTML = "Changed Text";
-}
-
-function LightOn() {
+function LightOn() {                                                                //change the src image
     document.getElementById("light").src = 'images/light_on.gif'
 }
 
-function LightOff() {
+function LightOff() {                                                               //change the src image
     document.getElementById("light").src = 'images/light_off.gif'
 }
 
+
 var fontsizeClicked = true;
 
-function ChangeFontSize() {
+function ChangeFontSize() {                                                         //toggles the font size of the element
     if (fontsizeClicked) {
         document.getElementById("fontsize").style.fontSize = "32px";
         fontsizeClicked = false;
@@ -23,7 +20,7 @@ function ChangeFontSize() {
     }
 }
 
-function HideElement() {
+function HideElement() {                                                            //toggles the visibility of the element
     var element = document.getElementById("hide");
 
     if (element.style.display == "none") {
@@ -33,6 +30,33 @@ function HideElement() {
     }
 }
 
-function Sum(a, b) {
+function Sum(a, b) {                                                                //adds 2 numbers
     document.getElementById("test").innerHTML = "2 + 2 = " + (a + b);
+}
+
+function Calculate() {                                                              //calculator, takes 2 numbers and an operator
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var operator = document.getElementById("op").value;
+    var answer = 0;
+
+    switch (operator) {
+        case "+":
+                answer = num1 + num2;
+            break;
+        case "-":
+                answer = num1 - num2;
+            break;
+        case "/":
+                answer = num1 / num2;
+            break;
+        case "*":
+                answer = num1 * num2;
+            break;
+        default:
+            break;
+    }
+
+    document.getElementById("answer").innerHTML = "Answer: " + answer;
+    console.log(answer);
 }
